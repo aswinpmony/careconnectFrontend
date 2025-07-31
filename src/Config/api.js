0881b0cd864c1795b91078fccd32f56b0dd779d6
aswinpmony <aswinpmony@gmail.com>
2025-07-31 17:03:49 +0530

    second commit

--- src/Config/api.js
+++ src/Config/api.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://192.168.1.72:5000";
-
+// const BASE_URL = "http://192.168.1.72:5000";
+const BASE_URL = "https://careconnectbackend-w424.onrender.com";
 export default BASE_URL;
